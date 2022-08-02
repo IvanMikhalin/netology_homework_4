49d4aa6ddc88456a9e8cf734bb6056fee671d714
Ivan Mikhalin <mikhalin_ivan@mail.ru>
2022-08-02 16:14:12 +0300

    code changed

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int result;
+        int milles = 20;
+        result = cost / milles;
+        return result;
+    }
+
+}
