356f48b8411cbfbe8e11a2643acef82d6f625c3e
Ivan Mikhalin <mikhalin_ivan@mail.ru>
2022-08-02 16:18:19 +0300

    new class added and changed code in main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticketprice = 3741; // стоимость биллета
-        int bonus_per_rubble = 20; // количества рублей для одной бонусной милли
-
-        int bonus = ticketprice / bonus_per_rubble; // переменная для количества бонусов
-
-        System.out.println("Стоимость билета: " + ticketprice + " руб.");
-        System.out.println("Вам начисленно бонусных милль: " + bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
